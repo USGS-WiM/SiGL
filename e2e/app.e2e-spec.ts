4dc49c5a411e248f4696a375a2e344e5dc00faa1
Tonia Roddick <troddick@USGS.gov>
2017-06-05 11:44:21 -0500

    initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SiglPage } from './app.po';
+
+describe('sigl App', () => {
+  let page: SiglPage;
+
+  beforeEach(() => {
+    page = new SiglPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
