a09a06b6df789fd65916681a01897443daea5a27
esmyers <emyers@usgs.gov>
2017-06-16 17:12:25 -0500

    selects in, state needs help

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -2,8 +2,18 @@ import { Component, Input, Output, EventEmitter, OnInit, ViewChild } from '@angu
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalService } from "app/shared/services/modal.service";
 import { SiglService } from "app/shared/services/siglservices.service";
-import { Iparameter } from "app/shared/interfaces/parameter.interface";
 import { IMultiSelectOption } from "angular-2-dropdown-multiselect";
+import { Iparameter } from "app/shared/interfaces/parameter.interface";
+import { IprojDuration } from "app/shared/interfaces/projduration.interface";
+import { IprojStatus } from "app/shared/interfaces/projstatus.interface";
+import { Iresource } from "app/shared/interfaces/resource.interface";
+import { Imedia } from "app/shared/interfaces/media.interface";
+import { Ilake } from "app/shared/interfaces/lake.interface";
+import { Istate } from "app/shared/interfaces/state.interface";
+import { ImonitorEffort } from "app/shared/interfaces/monitoreffort.interface";
+import { Iproject } from "app/shared/interfaces/project.interface";
+import { Iorganization } from "app/shared/interfaces/organization.interface";
+import { Iobjective } from "app/shared/interfaces/objective.interface";
 
 
 @Component({
@@ -15,9 +25,41 @@ export class FilterComponent implements OnInit {
   @ViewChild('filtermodal') public FilterComponent;  
   @Output() modalResponseEvent = new EventEmitter<boolean>(); // when they hit save, emit to calling component
   private modalElement: any;
-  public paramList: Array<Iparameter>;
+  //parameter
   public parameterMulti: Array<IMultiSelectOption>; //holds the parameter vals for the param groups
   public parameterSelected: Array<Number>;
+  //project Duration
+  public projDurationMulti: Array<IMultiSelectOption>;
+  public projDurationSelected: Array<Number>;
+  //project status
+  public projStatusMulti: Array<IMultiSelectOption>;
+  public projStatusSelected: Array<Number>;
+  //resource component
+  public resourceMulti: Array<IMultiSelectOption>;
+  public resourceSelected: Array<Number>;
+  //media
+  public mediaMulti: Array<IMultiSelectOption>;
+  public mediaSelected: Array<Number>;
+  //lake
+  public lakeMulti: Array<IMultiSelectOption>;
+  public lakeSelected: Array<Number>;
+  //state
+  public stateMulti: Array<IMultiSelectOption>;
+  public stateSelected: Array<Number>;
+  //monitoring effort
+  public monitoringEffortMulti: Array<IMultiSelectOption>;
+  public monitoringEffortSelected: Array<Number>;
+  //project
+   public projectMulti: Array<IMultiSelectOption>;
+  public projectSelected: Array<Number>;
+  //organization
+  public organizationMulti: Array<IMultiSelectOption>;
+  public orgainzationSelected: Array<Number>;
+  //objective
+  public objectiveMulti: Array<IMultiSelectOption>;
+  public objecttiveSelected: Array<Number>;
+
+
 
   //injects services into this component
   constructor(private _ngbService: NgbModal, private _modalService: ModalService, private _siglService: SiglService) { }
@@ -29,9 +71,69 @@ export class FilterComponent implements OnInit {
         params.forEach((p) => {
             this.parameterMulti.push({id:p.parameter_type_id, name:p.parameter});
         });
-
-
     });
+    this._siglService.projDurations.subscribe((durations: Array<IprojDuration>) => {
+        this.projDurationMulti = [];
+        durations.forEach((pd) => {
+            this.projDurationMulti.push({ id: pd.proj_duration_id, name: pd.duration_value });
+        });
+    });
+    this._siglService.projStatuses.subscribe((statuses: Array<IprojStatus>) => {
+        this.projStatusMulti = [];
+        statuses.forEach((ps) => {
+            this.projStatusMulti.push({ id: ps.proj_status_id, name: ps.status_value });
+        });
+    });
+    this._siglService.resources.subscribe((resources: Array<Iresource>) => {
+        this.resourceMulti = [];
+        resources.forEach((r) => {
+            this.resourceMulti.push({ id: r.resource_type_id, name: r.resource_name });
+        });
+    });
+    this._siglService.media.subscribe((media: Array<Imedia>) => {
+        this.mediaMulti = [];
+        media.forEach((m) => {
+            this.mediaMulti.push({ id: m.media_type_id, name: m.media });
+        });
+    });
+    this._siglService.lakes.subscribe((lakes: Array<Ilake>) => {
+        this.lakeMulti = [];
+        lakes.forEach((l) => {
+            this.lakeMulti.push({ id: l.lake_type_id, name: l.lake });
+        });
+    });
+    this._siglService.state.subscribe((states: Array<Istate>) => {
+        this.stateMulti = [];
+        states.forEach((s) => {
+            this.stateMulti.push({id: s.state_name, name: s.state_name });
+        });
+    });
+    this._siglService.monitorEffort.subscribe((monitorEfforts: Array<ImonitorEffort>) => {
+        this.monitoringEffortMulti = [];
+        monitorEfforts.forEach((me) => {
+            this.monitoringEffortMulti.push({id: me.monitoring_coordination_id, name: me.effort });
+        });
+    });
+    this._siglService.project.subscribe((projects: Array<Iproject>) => {
+        this.projectMulti = [];
+        projects.forEach((pr) => {
+            this.projectMulti.push({id: pr.project_id, name: pr.name });
+        });
+    });
+    this._siglService.organization.subscribe((organizations: Array<Iorganization>) => {
+        this.organizationMulti = [];
+        organizations.forEach((o) => {
+            this.organizationMulti.push({id: o.organization_id, name: o.organization_name });
+        });
+    });
+    this._siglService.objective.subscribe((objectives: Array<Iobjective>) => {
+        this.objectiveMulti = [];
+        objectives.forEach((ob) => {
+            this.objectiveMulti.push({id: ob.objective_type_id, name: ob.objective });
+        });
+    });
+
+    
     //show the filter modal == Change Filters button was clicked in sidebar
     this._modalService.showModal.subscribe((show: boolean) => {
         if (show) this.showFilterModal();            
--- src/app/shared/services/siglservices.service.ts
+++ src/app/shared/services/siglservices.service.ts
@@ -2,17 +2,31 @@ import { Injectable } from '@angular/core';
 import { Http,  Response, RequestOptions } from "@angular/http";
 import { Subject } from "rxjs/Subject";
 import { Iparameter } from "app/shared/interfaces/parameter.interface";
+import { IprojDuration } from "app/shared/interfaces/projduration.interface";
+import { IprojStatus } from "app/shared/interfaces/projstatus.interface";
+import { Iresource } from "app/shared/interfaces/resource.interface";
+import { Imedia } from "app/shared/interfaces/media.interface";
+
 import { Observable } from "rxjs/Observable";
 import { CONFIG } from "./config";
 
 import "rxjs/add/operator/map";
+import { Ilake } from "app/shared/interfaces/lake.interface";
+import { Istate } from "app/shared/interfaces/state.interface";
+import { ImonitorEffort } from "app/shared/interfaces/monitoreffort.interface";
+import { Iproject } from "app/shared/interfaces/project.interface";
+import { Iorganization } from "app/shared/interfaces/organization.interface";
+import { Iobjective } from "app/shared/interfaces/objective.interface";
+
+
+
 
 @Injectable()
 export class SiglService {
 
   constructor(private _http: Http) {
         this.getParameters();
-/*        this.getProjDurations();
+        this.getProjDurations();
         this.getProjStatuses();
         this.getResources();
         this.getMedia();
@@ -21,15 +35,55 @@ export class SiglService {
         this.getMonitorEfforts();
         this.getProjects();
         this.getOrganizations();
-        this.getObjectives();*/
+        this.getObjectives();
     }
 
     // subjects
     private _parameterSubject: Subject<Array<Iparameter>> = new Subject<Array<Iparameter>>();
+    private _projDurationSubject: Subject<Array<IprojDuration>> = new Subject<Array<IprojDuration>>();
+    private _projStatusSubject: Subject<Array<IprojStatus>> = new Subject<Array<IprojStatus>>();
+    private _resourceSubject: Subject<Array<Iresource>> = new Subject<Array<Iresource>>();
+    private _mediaSubject: Subject<Array<Imedia>> = new Subject<Array<Imedia>>();
+    private _lakeSubject: Subject<Array<Ilake>> = new Subject<Array<Ilake>>();
+    private _stateSubject: Subject<Array<Istate>> = new Subject<Array<Istate>>();
+    private _monitorEffortSubject: Subject<Array<ImonitorEffort>> = new Subject<Array<ImonitorEffort>>();
+    private _projectSubject: Subject<Array<Iproject>> = new Subject<Array<Iproject>>();
+    private _organizationSubject: Subject<Array<Iorganization>> = new Subject<Array<Iorganization>>();
+    private _objectiveSubject: Subject<Array<Iobjective>> = new Subject<Array<Iobjective>>();
     
     //getters
     public get parameters(): Observable<Array<Iparameter>> {
       return this._parameterSubject.asObservable();
+    }
+    public get projDurations(): Observable<Array<IprojDuration>> {
+      return this._projDurationSubject.asObservable();
+    }
+    public get projStatuses(): Observable<Array<IprojStatus>> {
+      return this._projStatusSubject.asObservable();
+    }
+    public get resources(): Observable<Array<Iresource>> {
+      return this._resourceSubject.asObservable();
+    }
+    public get media(): Observable<Array<Imedia>> {
+      return this._mediaSubject.asObservable();
+    }
+    public get lakes(): Observable<Array<Ilake>> {
+      return this._lakeSubject.asObservable();
+    }
+    public get state(): Observable<Array<Istate>> {
+      return this._stateSubject.asObservable();
+    }
+    public get monitorEffort(): Observable<Array<ImonitorEffort>> {
+      return this._monitorEffortSubject.asObservable();
+    }
+    public get project(): Observable<Array<Iproject>> {
+      return this._projectSubject.asObservable();
+    }
+    public get organization(): Observable<Array<Iorganization>> {
+      return this._organizationSubject.asObservable();
+    }
+    public get objective(): Observable<Array<Iobjective>> {
+      return this._objectiveSubject.asObservable();
     }
 
     //http requests
@@ -40,9 +94,92 @@ export class SiglService {
         .subscribe(p => {
             this._parameterSubject.next(p);
         }, error => this.handleError);
-      
+    }
+    private getProjDurations(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.PROJ_DURATIONS_URL, options)
+        .map(res => <Array<IprojDuration>>res.json())
+        .subscribe(pd => {
+            this._projDurationSubject.next(pd);
+        }, error => this.handleError);
+    }
+    private getProjStatuses(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.PROJ_STATUS_URL, options)
+        .map(res => <Array<IprojStatus>>res.json())
+        .subscribe(ps => {
+          this._projStatusSubject.next(ps);
+        }, error => this.handleError);
+    }
+    private getResources(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.RESOURCES_URL, options)
+        .map(res => <Array<Iresource>>res.json())
+        .subscribe(r => {
+          this._resourceSubject.next(r);
+        }, error => this.handleError);
+    }
+    private getMedia(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.MEDIA_URL, options)
+        .map(res => <Array<Imedia>>res.json())
+        .subscribe(m => {
+          this._mediaSubject.next(m);
+        }, error => this.handleError);
+    }
+    private getLakes(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.LAKES_URL, options)
+        .map(res => <Array<Ilake>>res.json())
+        .subscribe(l => {
+          this._lakeSubject.next(l);
+        }, error => this.handleError);
+    }
+    private getStates(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.STATES_URL, options)
+        .map(res => <Array<Istate>>res.json())
+        .subscribe(s => {
+          this._stateSubject.next(s);
+        }, error => this.handleError);
+    }
+    private getMonitorEfforts(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.MONITOR_EFFORTS_URL, options)
+        .map(res => <Array<ImonitorEffort>>res.json())
+        .subscribe(me => {
+          this._monitorEffortSubject.next(me);
+        }, error => this.handleError);
+    }
+    private getProjects(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.PROJECT_URL, options)
+        .map(res => <Array<Iproject>>res.json())
+        .subscribe(pr => {
+          this._projectSubject.next(pr);
+        }, error => this.handleError);
+    }
+    private getOrganizations(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.ORGANIZATION_URL, options)
+        .map(res => <Array<Iorganization>>res.json())
+        .subscribe(o => {
+          this._organizationSubject.next(o);
+        }, error => this.handleError);
+    }
+    private getObjectives(): void {
+      let options = new RequestOptions({ headers: CONFIG.MIN_JSON_HEADERS });
+      this._http.get(CONFIG.OBJECTIVE_URL, options)
+        .map(res => <Array<Iobjective>>res.json())
+        .subscribe(ob => {
+          this._objectiveSubject.next(ob);
+        }, error => this.handleError);
     }
 
+
+    
+
+    //Error Handler
     private handleError(error: Response){
       console.error(error);
       return Observable.throw(error.json().error || "Server Error");
