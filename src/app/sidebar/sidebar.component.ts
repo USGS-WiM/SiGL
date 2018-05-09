52f4c2a839775bd952ac2472e791d8bf38ab5eb1
esmyers <emyers@usgs.gov>
2018-05-09 09:46:20 -0500

    changes before pulling master

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -496,7 +496,7 @@ export class SidebarComponent implements OnInit {
 		var datePipe = new DatePipe("en-US");
 		var filteredDate = datePipe.transform(currentDate, 'yyyy-MM-dd HH:mm:ss Z');
 		var strDate = filteredDate.toString();
-		str = "#U.S. Geological Survey" + '\r\n' + "#Science in the Great Lakes (SiGL) Mapper"  + '\r\n' + "#https://sigl.wim.usgs.gov/sigl/" + '\r\n' + "#Retrieved: " + strDate + '\r\n' + '\r\n' + '"#SiGL project and site information is voluntarily provided and managed by federal and state agencies, municipalities, Tribes, universities, and nonprofit organizations. The USGS assumes no responsibility for the accuracy or completeness of information provided by other entities."' + '\r\n' + '\r\n' + "#Filters applied: " 
+		str = "#U.S. Geological Survey" + '\r\n' + "#Science in the Great Lakes (SiGL) Mapper"  + '\r\n' + "#https://sigldev.wim.usgs.gov/sigl/" + '\r\n' + "#Retrieved: " + strDate + '\r\n' + '\r\n' + '"#SiGL project and site information is voluntarily provided and managed by federal and state agencies, municipalities, Tribes, universities, and nonprofit organizations. The USGS assumes no responsibility for the accuracy or completeness of information provided by other entities."' + '\r\n' + '\r\n' + "#Filters applied: " 
 		//adding in applied filters in text heading
 		if (this.chosenFilters) {	
 			var filt = this.chosenFilters;
@@ -640,7 +640,7 @@ export class SidebarComponent implements OnInit {
 		var datePipe = new DatePipe("en-US");
 		var filteredDate = datePipe.transform(currentDate, 'yyyy-MM-dd HH:mm:ss Z');
 		var strDate = filteredDate.toString();
-		str = "#U.S. Geological Survey" + '\r\n' + "#Science in the Great Lakes (SiGL) Mapper"  + '\r\n' + "#https://sigl.wim.usgs.gov/sigl/" + '\r\n' + "#Retrieved: " + strDate + '\r\n' + '\r\n' + '"#SiGL project and site information is voluntarily provided and managed by federal and state agencies, municipalities, Tribes, universities, and nonprofit organizations. The USGS assumes no responsibility for the accuracy or completeness of information provided by other entities."' + '\r\n' + '\r\n' + "#Filters applied: " 
+		str = "#U.S. Geological Survey" + '\r\n' + "#Science in the Great Lakes (SiGL) Mapper"  + '\r\n' + "#https://sigldev.wim.usgs.gov/sigl/" + '\r\n' + "#Retrieved: " + strDate + '\r\n' + '\r\n' + '"#SiGL project and site information is voluntarily provided and managed by federal and state agencies, municipalities, Tribes, universities, and nonprofit organizations. The USGS assumes no responsibility for the accuracy or completeness of information provided by other entities."' + '\r\n' + '\r\n' + "#Filters applied: " 
 		//adding in applied filters in text heading
 		if (this.chosenFilters) {	
 			var filt = this.chosenFilters;
